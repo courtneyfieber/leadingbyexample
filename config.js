bb473a002f6668e27216d349de938136eaa82efc
Courtney Fieber <courtney.fieber@nrel.gov>
2021-08-04 07:51:37 -0600

    Add files via upload

--- config.js
+++ config.js
@@ -2,14 +2,14 @@ const config = {
   style: "mapbox://styles/mapbox/streets-v11",
   accessToken:
     "pk.eyJ1IjoiY2ZpZWJlciIsImEiOiJja3J3aG44MGIwZ3IzMm9vN2NvajczZzJwIn0.B9P8F_VRLzdMtl2TNgzZpg",
-  CSV: "leadingbyexample.csv",
-  center: [-77.0365581,38.8949924],
+  CSV: 'https://mizkortnie.github.io/leadingbyexample/leadingbyexample.csv',
+  center: [-71.109734,42.373615],
   zoom: 6,
   title: "Leading by Example",
   description:
     "Federal agencies across the country are taking action now to implement the President's Federal Sustainability Plan and Executive Order 14XX. View each project on the map to learn more.",
-  sideBarInfo: ["location_name", "description",],
-  popupInfo: ["location_name", "description",],
+  sideBarInfo: ["location_name", "description"],
+  popupInfo: ["location_name", "description"],
   filters: [
     {
       type: "dropdown",
