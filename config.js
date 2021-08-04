50b529221342fc8dbe3755b5c860d3035358df62
Courtney Fieber <courtney.fieber@nrel.gov>
2021-08-04 09:48:15 -0600

    Add files via upload

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
   style: "mapbox://styles/mapbox/streets-v11",
   accessToken:
-    "pk.eyJ1IjoiY2ZpZWJlciIsImEiOiJja3J3a3MzNXkwaG1oMnZwYW4wZjFnNnB1In0.VkdQ3fjQS1D5h0gb_HgNmQ",
+    "pk.eyJ1IjoiY2ZpZWJlciIsImEiOiJja3J3aG44MGIwZ3IzMm9vN2NvajczZzJwIn0.B9P8F_VRLzdMtl2TNgzZpg",
   CSV: "./leadingbyexample.csv",
   center: [-71.109734,42.373615],
   zoom: 6,
@@ -9,7 +9,7 @@ const config = {
   description:
     "Description",
   sideBarInfo: ["Location_Name", "Snippet"],
-  popupInfo: ["Location_Name", "Snippet"],
+  popupInfo: ["Location_Name"],
   filters: [
     {
       type: "dropdown",
@@ -22,7 +22,7 @@ const config = {
     {
       type: "dropdown",
       title: "Goal achieved: ",
-      columnHeader: "Type", // Case sensitive - must match spreadsheet entry
+      columnHeader: "Type",
       listItems: [
         "Carbon Pollution-Free Electricity", 
         "Zero-Emissions Fleet", 
@@ -34,7 +34,6 @@ const config = {
         "Environmental Justice and Equity", 
         "Domestic and Global Partnerships",
       ], 
-      // Case sensitive - must match spreadsheet entry; This will take up to six inputs but is best used with a maximum of three;
     },
     {
       type: "dropdown",
